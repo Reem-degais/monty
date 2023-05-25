43969cff07cb3d5245f14b6c4b12d7bb4c0e08c8
Reem Degais <reem.degais.33@gmail.com>
2023-05-25 10:52:35 -0700

    -

--- opcode3.c
+++ opcode3.c
@@ -25,3 +25,37 @@ void mul(stack_t **head, unsigned int count)
         *head = tmp->next;
         free(tmp);
 }
+
+/**
+ * pchar - prints the char at the top of the stack.
+ * @head: stack head.
+ * @count: line_number.
+ * Return: no return.
+ */
+void pchar(stack_t **head, unsigned int count)
+{
+	stack_t	*tmp = *head;
+
+
+	
+	if (tmp)
+	{
+		if (tmp->n > 127 || tmp->n < 0)
+		{
+			fprintf(stderr, "L%u: can't pchar, value out of range\n", count);
+			fclose(h.file);
+			free(h.buf);
+			free_stack(*head);
+			exit(EXIT_FAILURE);
+		}
+		fprintf(stdout, "%c\n", tmp->n);
+	}
+	else
+	{
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", count);
+		fclose(h.file);
+		free(h.buf);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+}
